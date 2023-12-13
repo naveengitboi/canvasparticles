b740936e2dfbc45d8215cdffb04ba832ee343969
naveengitboi <naveenjangiti9@gmail.com>
2023-12-13 13:26:02 +0530

    template done

--- app.js
+++ app.js
@@ -8,23 +8,34 @@ console.log(ctx)
 
 // setup classes
 
+
 class Particle{
     constructor(effect){
         this.effect = effect;
         this.radius = Math.random() * 40;
         this.x = this.radius + Math.random()*(this.effect.width - this.radius*2);
         this.y = this.radius + Math.random()*(this.effect.height - this.radius * 2);
+        this.velX = Math.random()*4 ;
+        this.velY = Math.random()*4 ;
     }
     
     draw(context){
         context.beginPath();
-        context.fillStyle = 'hsl(' + this.x + ', 100%, 50%';
+        context.fillStyle = 'hsl(' + 655 + ', 100%, 50%';
         context.arc(this.x, this.y, this.radius, 0, Math.PI*2);
         context.fill();
     }
 
     update(){
-        this.x ++;
+        this.x += this.velX;
+        this.y += this.velY
+        if(this.x >= this.effect.width || this.x <= 0) {
+            this.velX = -this.velX
+        }
+        if(this.y >= this.effect.height || this.y <= 0){
+            this.velY = -this.velY
+        }
+
     }
 }
 
@@ -65,6 +76,7 @@ function animate(){
     ctx.clearRect(0,0,canvas.width, canvas.height)
     effect.handleParticles(ctx)
     requestAnimationFrame(animate)
+
 }
 
 animate()
