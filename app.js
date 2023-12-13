71aadb3f583461e0f0b4eace1d30decf1fb888e5
naveengitboi <naveenjangiti9@gmail.com>
2023-12-13 17:10:56 +0530

    done with all physics in particles

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 ctx.fillStyle = "white"
+ctx.strokeStyle = "white"
+ctx.lineWidth = 1
 console.log(ctx)
 
 
@@ -12,11 +14,14 @@ console.log(ctx)
 class Particle{
     constructor(effect){
         this.effect = effect;
-        this.radius = 10;
+        this.radius = Math.floor(Math.random()*1);
         this.x = this.radius + Math.random()*(this.effect.width - this.radius*2);
         this.y = this.radius + Math.random()*(this.effect.height - this.radius * 2);
-        this.velX = Math.random()*4 -2 ;
-        this.velY = Math.random()*4 -2;
+        this.velX = Math.random()*1 - 0.5;
+        this.velY = Math.random()*1  - 0.5 ;
+        this.pushX = 0;
+        this.pushY = 0;
+        this.friction = 0.85;
     }
     
     draw(context){
@@ -27,14 +32,49 @@ class Particle{
     }
 
     update(){
-        this.x += this.velX;
-        this.y += this.velY
-        if(this.x >= this.effect.width || this.x <= 0) {
-            this.velX = -this.velX
+
+        if(this.effect.mouse.pressed){
+            let dx = this.x - this.effect.mouse.x;
+            let dy = this.y - this.effect.mouse.y;
+            const dist = Math.hypot(dx, dy);
+            const force = (this.effect.mouse.radius/dist)
+            if(dist <= this.effect.mouse.radius){
+                const angle = Math.atan2(dy,dx);
+                this.pushX += Math.cos(angle)*force;
+                this.pushY += Math.sin(angle)*force;
+            }
         }
-        if(this.y >= this.effect.height || this.y <= 0){
-            this.velY = -this.velY
+        this.x += (this.pushX *= this.friction) + this.velX;
+        this.y += (this.pushY *= this.friction) + this.velY;
+        if(this.x < this.radius){
+            this.x = this.radius;
+            this.velX *= -1;
         }
+        else if(this.x > this.effect.width - this.radius){
+            this.x = this.effect.width - this.radius;
+            this.velX *= -1;
+        }
+
+        if(this.y < this.radius){
+            this.y = this.radius;
+            this.velY *= -1;
+        }
+        else if(this.y > this.effect.height - this.radius){
+             this.y = this.effect.height - this.radius;
+            this.velY *= -1;
+        }
+
+  
+
+
+        // this.x += this.velX;
+        // this.y += this.velY
+        // if(this.x >= this.effect.width || this.x <= 0) {
+        //     this.velX = -this.velX
+        // }
+        // if(this.y >= this.effect.height || this.y <= 0){
+        //     this.velY = -this.velY
+        // }
 
     }
     reset(){
@@ -52,12 +92,40 @@ class Effect{
         this.height = this.canvas.height;
         this.context = context;
         this.particles = [];
-        this.numberOfParticles = 200;
+        this.numberOfParticles = 100;
         this.createParticles();
+
+        this.mouse = {
+            x:0,
+            y:0,
+            pressed : false,
+            radius: 300
+        }
+
         window.addEventListener('resize', (e) => {
             console.log(e)
             this.resize(e.target.window.innerWidth, e.target.window.innerHeight)
         })
+
+        window.addEventListener('mousemove', (e) => {
+            if(this.mouse.pressed == true){
+                this.mouse.x = e.x;
+                this.mouse.y = e.y;
+            }
+        })
+        window.addEventListener("mousedown", (e) => {
+            this.mouse.pressed = true;
+            this.mouse.x = e.x;
+            this.mouse.y = e.y;
+        })
+
+        window.addEventListener("mouseup", (e) => {
+            this.mouse.pressed = false;
+            this.mouse.x = e.x;
+            this.mouse.y = e.y;
+        })
+
+
         
 
     }
@@ -89,6 +157,7 @@ class Effect{
                     context.save()
                     const opacity = 1- (dist/maxDist);
                     context.globalAlpha = opacity;
+                    
                     context.beginPath();
                     context.moveTo(this.particles[i].x, this.particles[i].y);
                     context.lineTo(this.particles[j].x, this.particles[j].y);
